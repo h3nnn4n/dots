2981e60377c274bfb8ef99531ab9dd71ed7b6637
h3nnn4n <uber.renan@gmail.com>
2021-10-02 16:52:15 -0300

    Dont check if virtualenv wrapper exists

--- .config/zshrc.d/10-python.sh
+++ .config/zshrc.d/10-python.sh
@@ -7,9 +7,7 @@ export PATH="$HOME/.pyenv/shims:$PATH"
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 export WORKON_HOME=$HOME/.virtualenvs
 
-if [[ -a virtualenvwrapper_lazy ]]; then
-  pyenv virtualenvwrapper_lazy
-fi
+pyenv virtualenvwrapper_lazy
 
 # This has a tendency to make the shell explode, somehow
 # source $(which virtualenvwrapper.sh)
