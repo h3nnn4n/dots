5f94a390ba9f5b996da37cee79aa61540373aef9
h3nnn4n <git@renan.digital>
2022-07-29 13:29:54 -0300

    Remove virtual env stuff

--- .config/zshrc.d/10-python.sh
+++ .config/zshrc.d/10-python.sh
@@ -5,12 +5,11 @@ export PATH="$HOME/.pyenv/shims:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 eval "$(pipenv --completion)"
-export PATH="$HOME/.pyenv/shims:$PATH"
 
-export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+# export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 export WORKON_HOME=$HOME/.virtualenvs
 
-pyenv virtualenvwrapper_lazy
+# pyenv virtualenvwrapper_lazy
 
 # This has a tendency to make the shell explode, somehow
 # source $(which virtualenvwrapper.sh)
