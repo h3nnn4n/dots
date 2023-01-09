f6cbf62b50fe4dab1eee02b0f10b2f2324cdb518
h3nnn4n <uber.renan@gmail.com>
2023-01-09 08:54:05 -0300

    Detect os before setting python

--- .config/zshrc.d/10-python.sh
+++ .config/zshrc.d/10-python.sh
@@ -1,11 +1,18 @@
 # Python stuff
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-eval "$(pipenv --completion)"
-export PATH="$HOME/.pyenv/shims:$PATH"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  export PATH="$HOME/.pyenv/shims:$PATH"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+
+  eval "$(pyenv init -)"
+  eval "$(pyenv virtualenv-init -)"
+  eval "$(pipenv --completion)"
+fi
+
 
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
 export WORKON_HOME=$HOME/.virtualenvs
