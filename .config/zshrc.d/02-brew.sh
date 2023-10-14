b9a07ec1277efc707b5ff1c9fa19dc49913d8f98
h3nnn4n <uber.renan@gmail.com>
2023-10-14 16:30:17 -0300

    zsh: Only run homebrew related commands on osx

--- .config/zshrc.d/02-brew.sh
+++ .config/zshrc.d/02-brew.sh
@@ -1,5 +1,7 @@
-if ! command -v brew &> /dev/null
-then
-  eval "$(${HOME}/homebrew/bin/brew shellenv)"
-  export PATH="/Users/horse/homebrew/opt/mysql-client/bin:$PATH"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if ! command -v brew &> /dev/null
+  then
+    eval "$(${HOME}/homebrew/bin/brew shellenv)"
+    export PATH="/Users/horse/homebrew/opt/mysql-client/bin:$PATH"
+  fi
 fi
