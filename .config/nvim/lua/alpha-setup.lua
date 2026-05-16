3348e98bd5f6268977222d2386b836427b0c867e
Renan S Silva <renan@canals.ai>
2026-05-16 14:10:15 -0300

    Cheatsheet on landing page

--- .config/nvim/lua/alpha-setup.lua
+++ .config/nvim/lua/alpha-setup.lua
@@ -3,14 +3,18 @@ if not ok then return end
 
 local startify = require('alpha.themes.startify')
 
-local session_buttons = {
+local dashboard_buttons = {
   startify.button('s', '  Restore session for cwd',
     '<cmd>lua require("persistence").load()<cr>'),
   startify.button('S', '  Select session',
     '<cmd>lua require("persistence").select()<cr>'),
+  startify.button('?', '  Coding cheatsheet',
+    '<cmd>help coding-cheatsheet<cr>'),
+  startify.button('a', '  AI cheatsheet',
+    '<cmd>help ai-cheatsheet<cr>'),
 }
 
-for i, btn in ipairs(session_buttons) do
+for i, btn in ipairs(dashboard_buttons) do
   table.insert(startify.section.top_buttons.val, i, btn)
 end
 
