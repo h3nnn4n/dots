7f50e304070a56898aa42e64388093a3c6b9aed0
h3nnn4n <git@renan.digital>
2026-05-16 21:18:55 -0300

    Sync aliases

--- .config/zshrc.d/20-aliases.sh
+++ .config/zshrc.d/20-aliases.sh
@@ -1,10 +1,5 @@
 unset gs
 unset gl
-unset ll
-unset la
-
-alias ll="ls -lhrts"
-alias la="ls -lharts"
 
 # Utils
 alias todo=~/personal-todo/todo.sh
@@ -12,21 +7,13 @@ alias berta="time bundle exec ruby -Itest"
 alias pytest="pytest --pdbcls=IPython.core.debugger:Pdb -s"
 
 # Git stuff
-alias gl="git log --pretty=oneline --color | head -n 15"
-alias gla="git log --pretty=oneline --color"
+alias gl="git log --pretty=oneline --color | head -n15"
 alias gs="git status"
 alias gc="git commit"
 alias gd="git diff"
 alias gdc="git diff --cached"
 alias gap="git add --patch"
-alias gca="git commit --amend"
 alias gcan="git commit --amend --no-edit"
-alias gcnn="git commit --no-edit --no-verify"
-alias gb='git branch --sort=-committerdate --format="%(committerdate:relative)%09%(refname:short)" | head -n 15'
-alias gba='git branch --sort=-committerdate --format="%(committerdate:relative)%09%(refname:short)"'
-alias gck='git remote update && git checkout'
-alias gk='git checkout'
-alias gru='git remote update'
 
 # dotfile syncing
 alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
@@ -35,7 +22,6 @@ alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias tls="tmux list-sessions"
 alias ta="tmux attach -t"
 alias t="tmux"
-alias tks="tmux kill-session"
 
 tns () {
   SessionName=$1
@@ -49,5 +35,3 @@ tns () {
 # Random utils
 unset d
 alias d="say 'Done'"
-
-alias s="say"
